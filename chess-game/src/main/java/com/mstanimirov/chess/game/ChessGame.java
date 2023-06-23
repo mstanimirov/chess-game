2340f304b0c593db725dbbf1d2f2ef61374ecb10
mstanimirov <m.stanimirov81@gmail.com>
2023-06-23 19:11:32 +0300

    Initial Commit

---
+++ chess-game/src/main/java/com/mstanimirov/chess/game/ChessGame.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mstanimirov.chess.game;
+
+/**
+ *
+ * @author mstanimirov
+ */
+public class ChessGame {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
